236439c885ab9906f62b39e7b7b12e1ade899500
Bloom Perez <bloomdevelop@proton.me>
2025-04-20 15:27:08 -0400

    feat: website design overhaul

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,16 @@
 import * as v from "valibot";
 
-const LinksSchema = v.object({
-  title: v.string(),
-  url: v.optional(v.string()),
-  clipboard: v.optional(v.string()),
-});
+interface ILinks {
+  title: string;
+  url?: string;
+  clipboard?: string;
+}
 
-const ProjectsSchema = v.object({
+const RepoSchema = v.object({
   title: v.string(),
   description: v.string(),
   link: v.optional(v.string()),
 });
 
-export type LinksType = v.InferInput<typeof LinksSchema>;
-export type ProjectsType = v.InferInput<typeof ProjectsSchema>;
+export type LinksType = ILinks;
+export type RepoType = v.InferInput<typeof RepoSchema>;
