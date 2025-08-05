acfe7df1bcb046ba35d922613f4716ccc9341b97
Bloom Perez <springdotgay@tuta.io>
2025-08-04 21:16:58 -0400

    Added it's own page It's called "Spring's Testing Place", STP for short

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,51 @@
-const AUTHOR_NAME = "Spring"
+const AUTHOR_NAME = "Spring";
 
-export {AUTHOR_NAME}
+interface RoadmapArray {
+  description: string;
+  done: boolean;
+}
+
+const roadmap_array: RoadmapArray[] = [
+  {
+    description: "Transfur System",
+    done: false,
+  },
+  {
+    description: "Main Menu",
+    done: false,
+  },
+  {
+    description: "Health Bar",
+    done: true,
+  },
+  {
+    description: "Player List",
+    done: true,
+  },
+  {
+    description: "Shop UI",
+    done: false,
+  },
+  {
+    description: "Proper Lighting System",
+    done: true,
+  },
+  {
+    description: "Anti-Cheat (Separate Closed-Source)",
+    done: false,
+  },
+  {
+    description: "Combat System and Combat Logging",
+    done: false,
+  },
+  {
+    description: "Main Menu UI",
+    done: false,
+  },
+  {
+    description: "Inventory System",
+    done: false,
+  },
+];
+
+export { AUTHOR_NAME, roadmap_array };
