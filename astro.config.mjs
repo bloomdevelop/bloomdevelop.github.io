72a25477059df4495855966d6b384280358c605a
Bloom Perez <springdotgay@tuta.io>
2025-09-21 13:20:37 -0400

    wip: 2026 redesign

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
 
 import betterImageService from "astro-better-image-service";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()]
-  },
-
   integrations: [betterImageService()]
 });
