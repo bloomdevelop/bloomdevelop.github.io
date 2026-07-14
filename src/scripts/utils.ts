5e325de7d0d6aa6204f628761d59103c0164572e
Bloom Perez <valenplayer3000+code@gmail.com>
2026-07-14 18:32:17 -0400

    chore(lint): lint and format via biome

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,40 +1,40 @@
 export function extractRkeyFromPlainAtURI(uri: string) {
-  return uri.split("/").at(-1);
+	return uri.split("/").at(-1);
 }
 
 export function formatDate(date: string) {
-  const formatter = new Intl.DateTimeFormat("en-US", {
-    timeStyle: "long",
-    dateStyle: "short",
-  });
+	const formatter = new Intl.DateTimeFormat("en-US", {
+		timeStyle: "long",
+		dateStyle: "short",
+	});
 
-  return formatter.format(new Date(date));
+	return formatter.format(new Date(date));
 }
 
 export async function retry<T>(
-  fn: () => Promise<T>,
-  options: { initialDelay: number; maxRetries: number } = {
-    initialDelay: 200,
-    maxRetries: 10,
-  },
+	fn: () => Promise<T>,
+	options: { initialDelay: number; maxRetries: number } = {
+		initialDelay: 200,
+		maxRetries: 10,
+	},
 ): Promise<T> {
-  const { initialDelay, maxRetries } = options;
-  let attempt = 0;
+	const { initialDelay, maxRetries } = options;
+	let attempt = 0;
 
-  while (attempt <= maxRetries) {
-    try {
-      return await fn();
-    } catch (error) {
-      if (attempt === maxRetries) throw error;
+	while (attempt <= maxRetries) {
+		try {
+			return await fn();
+		} catch (error) {
+			if (attempt === maxRetries) throw error;
 
-      const t = initialDelay * Math.pow(2, attempt - 1);
-      const j = Math.random() * initialDelay;
-      const d = t + j;
+			const t = initialDelay * 2 ** (attempt - 1);
+			const j = Math.random() * initialDelay;
+			const d = t + j;
 
-      await new Promise<void>((r) => setTimeout(() => r(), d));
-      attempt++;
-    }
-  }
+			await new Promise<void>((r) => setTimeout(() => r(), d));
+			attempt++;
+		}
+	}
 
-  throw new Error("retry: exceeded maximum attempts");
+	throw new Error("retry: exceeded maximum attempts");
 }
